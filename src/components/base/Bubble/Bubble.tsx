459caedb3126546b6358eaeb2879b97f545dcad3
kokoball <kbp4154@naver.com>
2022-03-19 14:29:06 +0900

    :sparkles:  Feat: Bubble 컴포넌트 구현

---
+++ src/components/base/Bubble/Bubble.tsx
@@ -0,0 +1,35 @@
+import { ImageBox } from '@components/base';
+import * as S from './Style';
+
+export interface BubbleProps {
+  direction: {
+    leftAndRight: 'left' | 'right';
+    upAndDown: 'up' | 'down';
+  };
+  price: number;
+  discount: number;
+  imgURL: string;
+  title: string;
+}
+
+const Bubble = ({ direction, price, discount, imgURL, title }: BubbleProps) => {
+  return (
+    <S.BubbleBlock direction={direction}>
+      <ImageBox width={70} height={70} selected={false} radius={4} />
+      <S.RightSection>
+        <S.Title>{title}ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㄴㅇ</S.Title>
+        <S.PriceTab className="price-tab">
+          {discount ? (
+            <S.Discount>{discount}%</S.Discount>
+          ) : (
+            <S.ExpectedPrice>예상가</S.ExpectedPrice>
+          )}
+          <S.Price className={'price'}>{price}</S.Price>
+          <div>{'>'}</div>
+        </S.PriceTab>
+      </S.RightSection>
+    </S.BubbleBlock>
+  );
+};
+
+export default Bubble;
