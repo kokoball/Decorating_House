7ce2bb6208ec8a7a24e86d02caef84a1eb2f94ff
kokoball <kbp4154@naver.com>
2022-03-19 14:29:30 +0900

    :sparkles:  Feat: PictureViewer 컴포넌트 구현

---
+++ src/components/base/PictureViewer/PictureViewer.tsx
@@ -0,0 +1,7 @@
+import * as S from './Style';
+
+const PictureViewer = () => {
+  return <S.PictureViewerBlock />;
+};
+
+export default PictureViewer;
--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './Button/Button';
 export { default as ImageBox } from './ImageBox/ImageBox';
+export { default as Bubble } from './Bubble/Bubble';
+export { default as PictureViewer } from './PictureViewer/PictureViewer';
