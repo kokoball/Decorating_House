2de8c56ae0e64c1e8624d42be77dbcd5904014c8
kokoball <kbp4154@naver.com>
2022-03-21 21:57:25 +0900

    :sparkles:  Feat: dummy json 추가, types 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,16 @@
-// export { default as Temp } from './Temp';
+export interface IImageView {
+  id: number;
+  imageUrl: string;
+  productList: IProduct[];
+}
+export interface IProduct {
+  productId: number;
+  productName: string;
+  outside: boolean;
+  pointX: number;
+  pointY: number;
+  priceOriginal: number;
+  priceDiscount: number;
+  discountRate: number;
+  imageUrl: string;
+}
