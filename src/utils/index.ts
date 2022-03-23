cccd39bae425b1a2096e14a9a513d46684134ac6
kokoball <kbp4154@naver.com>
2022-03-23 20:40:25 +0900

    :sparkles:  Feat: slide 상수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,9 @@ export const addCommaSecond = (priceNumber: number) =>
   priceNumber.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
 
 export const IMAGE_WIDTH = 800;
+
+export const SLIDE = {
+  PAD: 20,
+  ITEM_WIDTH: 118,
+  SCROLL_COUNT: 7,
+};
